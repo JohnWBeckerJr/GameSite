de6c559bbd6ca630af1eaf9b67a7475937a56e8a
John William Becker Jr <Johnwilliambecker@gmail.com>
2021-01-18 16:20:47 -0500

    Large Commit/Server Branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//COMPONENTS & PAGES
+import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>SUP</h1> */}
+      <GlobalStyles />
+      <Route path={["/game/:id", "/"]}>
+        <Home />
+      </Route>
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,40 @@
+import React from "react";
+//STYLED COMP/FRAMER MOTION
+import styled from "styled-components";
+import { motion } from "framer-motion";
+//REDUX
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAction";
+import { Link } from "react-router-dom";
+
+const Game = ({ name, released, image, id }) => {
+  //LOAD DETAIL
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    dispatch(loadDetail(id));
+  };
+  return (
+    <StyledGame onClick={loadDetailHandler}>
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released}</p>
+        <img src={image} alt="{name}"></img>
+      </Link>
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.5);
+  text-align: center;
+  border-radius: 1rem;
+
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
+`;
+
+export default Game;
